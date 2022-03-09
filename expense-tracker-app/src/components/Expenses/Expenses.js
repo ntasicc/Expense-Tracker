6eda2f8c31215a9d54f18310cfa1183b0ae56838
ntasicc <ntasicc@gmail.com>
2022-03-09 18:42:21 +0100

    Rendering expense filter and collecting data from it

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -1,10 +1,24 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import "./Expenses.css";
 import Card from "../Card/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  console.log(filteredYear);
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.items.map((el) => (
         <ExpenseItem
           key={el.id}
